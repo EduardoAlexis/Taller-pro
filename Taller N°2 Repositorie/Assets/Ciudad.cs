75564fb647ad6b60bb08ace4fbab30a2182d18e8
EduardoAlexis <alexis-2712@hotmail.com>
2019-04-30 07:07:07 -0500

    Update comentado
    Cogigo comentado

--- Taller N°2 Repositorie/Assets/Ciudad.cs
+++ Taller N°2 Repositorie/Assets/Ciudad.cs
@@ -9,13 +9,13 @@ public class Ciudad : MonoBehaviour
 
      void Start()
     {
-        heroe = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        heroe.AddComponent<Heroe>();       
+        heroe = GameObject.CreatePrimitive(PrimitiveType.Sphere); //Esto es para crear una primitiva del heroe y se hace en el start para que solo se cree una vez iniciado
+        heroe.AddComponent<Heroe>();       //Aca se le añade el componente de gameobject
 
-        for(int i = 0; i < 11; i++)
+        for(int i = 0; i < 11; i++) // con el for se crea un ciclo para la instancia de los cubos
         {
             Cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-			int CorZ = Random.Range(0, 2);
+			int CorZ = Random.Range(0, 2);  // esto se usa para dar un rango entre 0 y 1 para posteriormente asignarle un componente de zombie o ciudadano
             if (CorZ == 0)
             {
                 Cube.AddComponent<Zoombie>();
